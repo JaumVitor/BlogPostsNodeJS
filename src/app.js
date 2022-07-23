7f9ba9fa635a3f83a8a0f3cce6a96a9223d39956
Jaum Vitor <joao.vitor.pires.060@gmail.com>
2022-07-23 00:31:24 -0300

    create, list categories and validate input form with messge err or success

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
+// Usando exports de rotas, para vizualização de endpoints
+const admin = require('./routes/admin')
 
 // Configurando view engine
 app.set('view engine', 'ejs')
@@ -12,12 +17,34 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.json())
 
-// Usando exports de rotas, para vizualização de endpoints
-const admin = require('./routes/admin')
+app.use(
+  session({
+    secret: 'secretSession',
+    saveUninitialized: true,
+    resave: true
+  })
+)
+
+app.use(flash())
+
+// Criando meu middleware para criar variaveis globais que serão usadas pelo flash
+app.use('/', (req, res, next) => {
+  // Prosseguindo apos definição das variaveis
+  res.locals.err = req.flash('err')
+  res.locals.success = req.flash('success')
+  next()
+})
+
+app.get('/', (req, res) => {
+  req.flash('flash', 'Testando outro valor')
+  console.log(res.locals.flashUse)
+  res.send('teste')
+})
 
 // Definindo os endpoints da aplicação
-app.get('/', (req, res) => {
-  res.send('Rota definida')
+app.post('/', (req, res) => {
+  //  { message: 'teste', outro: 1 })
+  // res.redirect('/')
 })
 
 app.use('/admin', admin)
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,9 +11,9 @@ const CategoryModel = require('../database/models/category.model')
 router.get('/', (req, res) => {
   res.render('admin/index')
 })
-
+;('')
 router.get('/categories', async (req, res) => {
-  const category = await CategoryModel.find({})
+  const category = await CategoryModel.find({}).sort({ date: 'desc' })
 
   res.render('admin/categories', { category })
 })
@@ -22,19 +22,63 @@ router.get('/categories/add', (req, res) => {
   res.render('admin/categoriesAdd')
 })
 
-router.post('/categories/add/new', async (req, res) => {
-  const category = {
-    name_category: req.body.name,
-    slug: req.body.slug
+router.get('/categories/add/new', (req, res) => {
+  console.log(res.locals.err)
+  res.render('admin/categoriesAdd')
+})
+
+router.post('/categories/add', async (req, res) => {
+  // Validando inputs do formulario, caso haja erro seta valor no array de erros
+  const err = []
+  if (
+    !req.body.name ||
+    typeof req.body.name == undefined ||
+    req.body.name == null
+  ) {
+    err.push({
+      message: 'Nome invalido!'
+    })
   }
 
-  await CategoryModel.create(category)
-    .then(() => {
-      res.redirect('/admin/categories')
+  if (
+    !req.body.slug ||
+    typeof req.body.slug == undefined ||
+    req.body.slug == null
+  ) {
+    err.push({
+      message: 'Slug invalido!'
     })
-    .catch(() => {
-      console.log('Categoria não criada' + err.message)
+  }
+
+  if (err.length > 0) {
+    // Caso exista erros adiciona todos nas variaveis globais
+    err.push({
+      message: 'Categoria não foi cadastrada!'
     })
+    // Inserindo erros na variavel global de erros, definida no meu middleware
+    err.forEach(e => {
+      req.flash('err', e)
+    })
+
+    res.redirect('/admin/categories/add')
+  } else {
+    // Caso contrario salva valores no banco
+    const category = {
+      name_category: req.body.name,
+      slug: req.body.slug
+    }
+
+    // Categoria cadastrada com sucesso
+    await CategoryModel.create(category)
+      .then(() => {
+        req.flash('success', 'Categoria criada com sucesso!')
+        res.redirect('/admin/categories')
+      })
+      // Categoria não foi cadastrada
+      .catch(err => {
+        console.log('Categoria não criada' + err.message)
+      })
+  }
 })
 
 module.exports = router
